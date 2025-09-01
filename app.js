b30c3650676b54eb0d6a48ed26f4be63f963d4dd
Abhinav Anand <abhinav011501@gmail.com>
2025-09-01 12:53:56 +0530

    Update: change default server port from 5000 to 3000 in app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import { fetchAndStoreVideos } from "./controller/video.controller.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 connectDB();
